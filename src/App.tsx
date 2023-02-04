0a93ffa6d181200e868b38610e66ca1f3f21199d
EtienneDuv <etienne.duverney@gmail.com>
2023-02-04 15:02:52 +0100

    [Add] Basic setup + SVG type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../public/logo.svg';
+import {logo} from './svg';
 
 export default () => {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo.toString()} className="App-logo" alt="logo" />
                 <p>
           Edit <code>src/App.js</code> and save to reload.
                 </p>
